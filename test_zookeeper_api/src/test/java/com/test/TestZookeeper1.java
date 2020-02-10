526c24025126f5b81cb651722931b749486be16c
itcast <1242377231@qq.com>
2020-02-10 18:22:02 +0800

    节点修改,查询,删除

--- test_zookeeper_api/src/test/java/com/test/TestZookeeper1.java
+++ test_zookeeper_api/src/test/java/com/test/TestZookeeper1.java
@@ -21,7 +21,7 @@ public class TestZookeeper1 {
         /**
          * newClient(String connectString, int sessionTimeoutMs, int connectionTimeoutMs, RetryPolicy retryPolicy)
          *      String connectString：连接Zookeeper的地址：127.0.0.1:2181
-         *      int sessionTimeoutMs：绘画Session的超时时间(单位秒)，3000
+         *      int sessionTimeoutMs：会话Session的超时时间(单位秒)，3000
          *      int connectionTimeoutMs：连接的超时时间(单位秒)，3000
          *      RetryPolicy retryPolicy：重试策略
          */
@@ -54,6 +54,58 @@ public class TestZookeeper1 {
         //5.2设置延时5秒关闭（Thread.sleep(5000)）
         Thread.sleep(10000);
 
+        //关闭
+        client.close();
+    }
+
+    //修改节点
+    @Test
+    public  void updateNode() throws Exception{
+        //定义重试策略
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(3000,3);
+
+        CuratorFramework client = CuratorFrameworkFactory.newClient("127.0.0.1:2181", 3000, 3000, retryPolicy);
+        //开启
+        client.start();
+        //修改节点
+        client.setData().forPath("/bb","haha".getBytes());
+        //关闭
+        client.close();
+    }
+    //查询节点
+    @Test
+    public void queryNode() throws Exception{
+        //定义重试策略
+        RetryPolicy retryPolicy =new ExponentialBackoffRetry(3000,3);
+        CuratorFramework client = CuratorFrameworkFactory.newClient("127.0.0.1:2181", 3000, 3000, retryPolicy);
+        //开启
+        client.start();
+        //查询节点
+        byte[] bytes = client.getData().forPath("/ff/f_1");
+        //转成字符串格式打印出来
+        System.out.println(new String(bytes));
+        //关闭
+        client.close();
+    }
+
+    //删除节点
+    @Test
+    public void deleteNode() throws Exception{
+        RetryPolicy retryPolicy=new ExponentialBackoffRetry(3000,3);
+        CuratorFramework client = CuratorFrameworkFactory.newClient("127.0.0.1:2181", 3000, 3, retryPolicy);
+        //开启
+        client.start();
+        //1.删除没有子的节点
+       // client.delete().forPath("/a");
+        //2.删除有子的节点(.deletingChildrenIfNeeded():表示递归删除根节点下对应的子节点)
+        //client.delete().deletingChildrenIfNeeded().forPath("/ff");
+        /**
+         * .guaranteed()：
+         *  1.强制保证删除一个节点
+         *  2.只要客户端会话有效,那么Curator会再后台持续进行删除操作，知道节点删除成功/
+         *  3。比如遇到一些网络异常的情况，此guaranteed的强制删除就会很有效。
+         */
+        client.delete().guaranteed().deletingChildrenIfNeeded().forPath("/cc");
         //关闭
         client.close();
     }
