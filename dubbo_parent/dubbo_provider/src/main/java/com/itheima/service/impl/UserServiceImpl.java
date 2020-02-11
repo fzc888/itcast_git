d36b9f4789f89fd0b65655a2efab5b4570b2a8c2
itcast <1242377231@qq.com>
2020-02-11 19:25:05 +0800

    初始化java资源目录

---
+++ dubbo_parent/dubbo_provider/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.itheima.service.impl;
+
+import com.itheima.dao.UserDao;
+import com.itheima.pojo.User;
+import com.itheima.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserServiceImpl implements UserService {
+    @Autowired
+    UserDao userDao;
+
+    @Override
+    public User findById(Integer id) {
+        User user = userDao.findById(id);
+        return user;
+    }
+}
