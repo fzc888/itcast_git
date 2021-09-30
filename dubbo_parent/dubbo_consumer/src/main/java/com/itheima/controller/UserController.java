d34ac4918fae9e87cc226dca20321104cf1e6eec
itcast <1242377231@qq.com>
2021-09-30 19:47:14 +0800

    eeee

--- dubbo_parent/dubbo_consumer/src/main/java/com/itheima/controller/UserController.java
+++ dubbo_parent/dubbo_consumer/src/main/java/com/itheima/controller/UserController.java
@@ -1,17 +1,21 @@
 package com.itheima.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.itheima.pojo.User;
 import com.itheima.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@ResponseBody//响应jsons数据到页面
+/*@Controller
+@ResponseBody//响应jsons数据到页面*/
+@RestController //等价于上面两个注解
 @RequestMapping(value = "/user")
 public class UserController {
-    @Autowired
+    // @Autowired
+    @Reference //订阅
     UserService userService;
 
     //主键查询
--- dubbo_parent/dubbo_provider/src/main/java/com/itheima/service/impl/UserServiceImpl.java
+++ dubbo_parent/dubbo_provider/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -1,11 +1,13 @@
 package com.itheima.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.itheima.dao.UserDao;
 import com.itheima.pojo.User;
 import com.itheima.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+@Service(protocol = "rmi")
 @Transactional
 public class UserServiceImpl implements UserService {
     @Autowired
